7fc2d4a2326c1a486def9a77f288e5647375593e
Shuvoghosh7 <shuvoghosh1610@gmail.com>
2022-05-01 13:26:47 +0600

    add navber

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
 import logo from './logo.svg';
 import './App.css';
+import { Route, Routes } from 'react-router-dom';
+import Header from './component/Share/Header/Header';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
   return (
-    <div className="App">
-      <h1>Hello</h1>
+    <div >
+      <Header/>
+      <Routes>
+        
+      </Routes>
     </div>
   );
 }
