bcafa855b0ee25b9dff9155bb1b393e5686a534d
Shuvoghosh7 <shuvoghosh1610@gmail.com>
2022-05-06 00:03:09 +0600

    add notFound page

--- src/component/NotFound/NotFound.js
+++ src/component/NotFound/NotFound.js
@@ -1,13 +1,19 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-
+import errors404 from '../../images/errors404.jpg'
 const NotFound = () => {
     return (
-        <div>
+        <div className='d-lg-flex justify-content-between mt-5 container'>
+            <div className='d-flex justify-content-center align-items-center' >
+                <div>
+                    <h1 className='text-primary'>Oops!</h1>
+                    <h4 className='text-danger'>The page you're looking for isn't here.</h4>
+                    <h5> You might have the wrong address, or the page may have moved.</h5>
+                    <button className='btn btn-outline-success rounded-pill '><Link className='text-decoration-none text-dark' to="/">Go To Homepage</Link></button>
+                </div>
+            </div>
             <div>
-                <h1>Oops!</h1>
-                <p>404-Page Not Found</p>
-                <button className='btn btn-outline-success rounded-pill text-de'><Link to="/">Go To Homepage</Link></button>
+                <img style={{height:"400px"}} className='img-fluid' src={errors404} alt="" />
             </div>
         </div>
     );
